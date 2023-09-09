914887eca270db2ec0d3d6753cb90d8b6077b1e7
cha2hyun <cha2hyun.dev@gmail.com>
2023-09-09 15:27:26 +0900

    #1.2 Variables and Constants

--- main.go
+++ main.go
@@ -5,6 +5,16 @@ import (
 )
 
 // main 은 컴파일을 위해서 만들어지는 파일 optional 아님
+// name := "soohyun" GO가 타입을 알아서 찾아줌
+// name := "soohyun"  ==  var name string = "soohyun"
+// 하지만 축약형은 오로지 func 안에서만 작동, variables 만 const는 불가
 func main() {
 	fmt.Println("hello world")
+	const name string = "soohyun"
+
+	fmt.Println(name)
+	var name1 string = "sohyun"
+	name2 := "sohyun"
+	fmt.Println(name2)
+
 }
