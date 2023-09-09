e79de6dfae89081e53c4ad105a5d876bd991569f
cha2hyun <cha2hyun.dev@gmail.com>
2023-09-09 15:35:04 +0900

    #1.4 Functions part one

--- main.go
+++ main.go
@@ -2,19 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // main 은 컴파일을 위해서 만들어지는 파일 optional 아님
 // name := "soohyun" GO가 타입을 알아서 찾아줌
 // name := "soohyun"  ==  var name string = "soohyun"
 // 하지만 축약형은 오로지 func 안에서만 작동, variables 만 const는 불가
+// func main() {
+// 	fmt.Println("hello world")
+// 	const name string = "soohyun"
+
+// 	fmt.Println(name)
+// 	var name1 string = "sohyun"
+// 	name2 := "sohyun"
+// 	fmt.Println(name2)
+// }
+
+// func multiply(a int, b int) int {
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string){
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 func main() {
-	fmt.Println("hello world")
-	const name string = "soohyun"
-
-	fmt.Println(name)
-	var name1 string = "sohyun"
-	name2 := "sohyun"
-	fmt.Println(name2)
-
+	fmt.Println(multiply(2, 2))
+	fmt.Println(lenAndUpper("cha2hyun"))
+	totalLength, upperName := lenAndUpper("cha2hyun")
+	fmt.Println(totalLength, upperName )
+	repeatMe("1","2","3","4")
 }
